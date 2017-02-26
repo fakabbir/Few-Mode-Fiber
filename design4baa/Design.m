34c9d774d1b03f1c67e1079fe3d2558559db8996
Fakabbir Amin <fakabbir@gmail.com>
2017-02-26 12:32:39 +0530

    Make plot for all graphs,TODO finishing and pasting to ppt

--- design4baa/Design.m
+++ design4baa/Design.m
@@ -1,70 +1,51 @@
-
-d=0.7*10;
-x_init=linspace(0,d,50);
-x_final=linspace(62.5-30,62.5,50);
-x_mid=linspace(d,62.5-30,50);
+format long;
 n_clad=1.4444;
 delta_val=.80;
-n_core=n_clad*sqrt((100)/(100-2*delta_val));
+half_delta_val=0.40;
+n_corea=n_clad*sqrt((100)/(100-2*delta_val));
+n_coreb=n_clad*sqrt((100)/(100-2*half_delta_val));
 
-
-k1=1;
-k2=2;
-k3=3;
-k4=5;
-l_max=n_core-n_clad;
-x_plot=linspace(-6,6,50);
+total_dia=62.5;
+d=3;
+%n0=[n_clad n_corea n_coreb n_corea n_clad];
+%a=[d+k_stepness 10 10 10 32.5-d-k_stepness]*1e-6;
 figure
-subplot(1,2,1)
-n_mids=(l_max./(1+exp(-k1*(x_plot))))+n_clad;
-n_mids2=(l_max./(1+exp(-k2*(x_plot))))+n_clad;
-n_mids3=(l_max./(1+exp(-0.9*(x_plot))))+n_clad;
-plot(linspace(0,7/2,50),ones(1,50)*n_core)
+subplot(3,1,1)
+plot(linspace(0,d,50),ones(1,50)*n_clad);
 hold on
-plot(ones(1,50)*7/2,linspace(n_core,n_clad,50));
-plot(linspace(7/2,7,50),ones(1,50)*n_clad)
-plot(x_mid,n_mids)
-plot(x_final,ones(1,50)*n_clad)
-plot(ones(1,50)*x_mid(end),linspace(n_core,n_clad,50))
-xlabel('Radius in micrometer')
-ylabel('Refractive index,N')
-title('Design of fiber')
-grid on
-
-plot(x_mid,n_mids2)
-plot(x_mid,n_mids2)
-plot(x_mid,n_mids3)
-
-d=0.7*10;
-x_init=linspace(0,d,50);
-x_final=linspace(62.5-30,62.5,50);
-x_mid=linspace(d,62.5-30,50);
-n_clad=1.4444;
-delta_val=.80;
-n_core=n_clad*sqrt((100)/(100-2*delta_val));
+plot(ones(1,50)*d,linspace(n_clad,n_corea,50));
+plot(linspace(d,d+10,50),ones(1,50)*n_corea);
+plot(ones(1,50)*(d+10),linspace(n_corea,n_coreb,50));
+plot(linspace(d+10,d+20,50),ones(1,50)*n_coreb);
+plot(ones(1,50)*d+20,linspace(n_coreb,n_corea,50));
+plot(linspace(d+20,d+30,50),ones(1,50)*n_corea);
+plot(ones(1,50)*(d+30),linspace(n_corea,n_clad,50));
+plot(linspace(d+30,62.5,50),ones(1,50)*n_clad);
+title('Fiber Design ABA')
 
 
-k1=1;
-k2=2;
-k3=3;
-k4=5;
-l_max=n_core-n_clad;
-x_plot=linspace(-6,6,50);
-subplot(1,2,2)
-n_mids=(l_max./(1+exp(k1*(x_plot))))+n_clad;
-n_mids2=(l_max./(1+exp(k2*(x_plot))))+n_clad;
-n_mids3=(l_max./(1+exp(0.9*(x_plot))))+n_clad;
-plot(x_mid,n_mids)
+subplot(3,1,2)
+plot(linspace(0,d,50),ones(1,50)*n_clad);
 hold on
-plot(linspace(0,7/2,50),ones(1,50)*n_core)
-plot(ones(1,50)*7/2,linspace(n_core,n_clad,50))
-plot(linspace(7/2,7,50),ones(1,50)*n_clad)
-plot(ones(1,50)*7,linspace(n_core,n_clad,50))
-plot(linspace(x_mid(end),62.5,50),ones(1,50)*n_clad)
-xlabel('Radius in micrometer')
-ylabel('Refractive index,N')
-title('Design of fiber')
-grid on
+plot(ones(1,50)*d,linspace(n_clad,n_corea,50));
+plot(linspace(d,d+20,50),ones(1,50)*n_corea);
+%plot(ones(1,50)*(d+10),linspace(n_corea,n_coreb,50));
+%plot(linspace(d+10,d+20,50),ones(1,50)*n_coreb);
+plot(ones(1,50)*d+20,linspace(n_corea,n_coreb,50));
+plot(linspace(d+20,d+30,50),ones(1,50)*n_coreb);
+plot(ones(1,50)*(d+30),linspace(n_coreb,n_clad,50));
+plot(linspace(d+30,62.5,50),ones(1,50)*n_clad);
+ylabel('Refractive Index N')
 
-
-plot(x_mid,n_mids3)
+subplot(3,1,3)
+plot(linspace(0,d,50),ones(1,50)*n_clad);
+hold on
+plot(ones(1,50)*d,linspace(n_clad,n_coreb,50));
+plot(linspace(d,d+10,50),ones(1,50)*n_coreb);
+plot(ones(1,50)*(d+10),linspace(n_coreb,n_corea,50));
+plot(linspace(d+10,d+30,50),ones(1,50)*n_corea);
+plot(ones(1,50)*d+30,linspace(n_coreb,n_corea,50));
+%plot(linspace(d+20,d+30,50),ones(1,50)*n_corea);
+plot(ones(1,50)*(d+30),linspace(n_corea,n_clad,50));
+plot(linspace(d+30,62.5,50),ones(1,50)*n_clad);
+xlabel('Radius in Micrometer')
